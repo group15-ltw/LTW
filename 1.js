36954336bee0744ed756ba604d105812068f568b
Thao Lac <14110180@student.hcmute.edu.vn>
2016-09-29 10:32:32 +0700

    Lan dua len dau tien

---
+++ 1.js
@@ -0,0 +1,11 @@
+﻿
+$(window).load(function(){
+$dropdown = $("#contextMenu");
+$(".dsDeTai_actionButton").click(function() {
+    var id = $(this).closest("tr").children().first().html();
+    $(this).after($dropdown);
+    $dropdown.find(".payLink").attr("href", "/transaction/pay?id="+id);
+    $dropdown.find(".delLink").attr("href", "/transaction/delete?id="+id);
+    $(this).dropdown();
+});
+});//]]>  
